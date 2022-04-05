cf2941c60711f95f883dcb030b78a22cccb9a89a
symphony <jciurc@gmail.com>
2022-04-05 03:45:42 +0000

    created custom assert equal function

---
+++ assertEqual.js
@@ -0,0 +1,19 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  // Create console messages
+  const isEqual = actual === expected;
+  const failedMsg = "🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected;
+  const passedMsg = "✅✅✅ Assertion Passed: " + actual + " === " + expected;
+
+  // test and return appropriate response
+  if (!isEqual) console.log(failedMsg);
+  else console.log(passedMsg);
+};
+
+// TEST CODE
+assertEqual(1, "1");
+assertEqual("well, well, well", "whale, whale, whale");
+assertEqual("HELLO WORLD".toLowerCase(), "hello world");
+assertEqual(13 + 8, 7 * 3);
+assertEqual(undefined, 0);
+assertEqual(!NaN, "this is getting complex" > "?");
