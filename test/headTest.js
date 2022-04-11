1151f3f451be4e8c704e18ab5100c5d8ba6d054b
symphony <jciurc@gmail.com>
2022-04-11 16:12:53 -0600

    Installed m&c and set up headTest.js to use Mocha

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,17 @@
-const assertEqual = require('../assertEqual').assertEqual;
+const assert = require('chai').assert;
 const head = require("../head.js");
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([15]), 15);
-assertEqual(head([]), undefined);
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  }),
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+});
+
+// Additional
+// head([5,6,7]), 5);
+// head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// head([15]), 15);
+// head([]), undefined);
