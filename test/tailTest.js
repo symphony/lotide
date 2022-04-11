d1c84d7ab9dbe4d4a4b5bc6cc152d3c0090df95a
symphony <jciurc@gmail.com>
2022-04-11 16:22:15 -0600

    Set up tailTest.js to incorporate mocha testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,19 @@
-const assertEqual = require('../assertEqual').assertEqual;
+const assert = require('chai').assert;
 const tail = require("../tail");
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// Test tail edge cases
-assertEqual(tail([1, 4]), 4);
-assertEqual(tail([1, 4]), [4, 3, 2]);
-assertEqual(tail([1]), []);
+describe("#head", () => {
+  it("should return not mutate the original array", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.strictEqual(words.length, 3);
+  }),
+  it("returns [4] for [1, 4]", () => {
+    assert.deepEqual(tail([1, 4]), [4]);
+  }),
+  it("returns [3, 2, 4] for [1, 3, 2, 4]", () => {
+    assert.deepEqual(tail([1, 3, 2, 4]), [3, 2, 4]);
+  }),
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+});
