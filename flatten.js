8cee6277e455d2eb8cb961bf38cc183f15a788ae
symphony <jciurc@gmail.com>
2022-04-16 21:31:48 -0600

    improved flatten.js. doesn't make a new array each iteration now

--- flatten.js
+++ flatten.js
@@ -1,9 +1,6 @@
 const {assertEqual} = require('./assertEqual');
-
 const flatten = (array) => {
-  let squashed = [];
-  array.forEach((item) => {squashed = squashed.concat(item);});
-  return squashed;
+  return array.reduce((acc, item) => acc.concat(item), []);
 };
 
 assertEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
