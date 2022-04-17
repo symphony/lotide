8de4bb252ba48505cd2e4a88c6dba5c68aac7a1e
symphony <jciurc@gmail.com>
2022-04-16 21:45:23 -0600

    Updated to use recursion and can flatten up to any depth

--- flatten.js
+++ flatten.js
@@ -1,6 +1,6 @@
 const {assertEqual} = require('./assertEqual');
 const flatten = (array) => {
-  return array.reduce((acc, item) => acc.concat(item), []);
+  return array.reduce((acc, item) => acc.concat(Array.isArray(item) ? flatten(item) : item), []);
 };
 
-assertEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+assertEqual(flatten([1, [2, [3], [4, [[[5, [6]]]]]]]), [1, 2, 3, 4, 5, 6]);
